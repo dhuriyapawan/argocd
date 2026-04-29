aa1bee9fad03890ae48091725c6726a7c3bc59f2
pawandhuriya@outlook.com <pawan.n.dhuriya@currentware.com>
2026-04-29 15:57:36 +0530

    first commit

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -21,7 +21,7 @@ resource "aws_eks_cluster" "cluster" {
 resource "aws_eks_node_group" "ng" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "default-ng"
-  node_role       = aws_iam_role.eks_node_role.arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.private[*].id
   scaling_config {
     desired_size = 2
