c744db9b1e70de2e579624ec864df8a2752b49e6
pawan <dhuriya.pawan@gmail.com>
2026-04-30 14:52:01 +0530

    terraform apply

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,56 +1,62 @@
-# Use an existing VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-data "aws_vpc" "main" {
-  id = "vpc-036750db266014be4"
-}
-
-# Internet gateway attached to the existing VPC
-resource "aws_internet_gateway" "igw" {
-  vpc_id = data.aws_vpc.main.id
   tags = {
-    Name = "eks-igw"
+    Name = "${var.project}-vpc"
   }
 }
 
-# Public subnets (created in the existing VPC)
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project}-igw"
+  }
+}
+
 resource "aws_subnet" "public" {
   count                   = length(var.azs)
-  vpc_id                  = data.aws_vpc.main.id
-  cidr_block              = element(var.public_subnet_cidrs, count.index)
-  availability_zone       = element(var.azs, count.index)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
   map_public_ip_on_launch = true
+
   tags = {
-    Name = "eks-public-${count.index}"
+    Name = "${var.project}-public-${count.index}"
+    "kubernetes.io/role/elb" = "1"
   }
 }
 
-# Private subnets (created in the existing VPC)
 resource "aws_subnet" "private" {
   count             = length(var.azs)
-  vpc_id            = data.aws_vpc.main.id
-  cidr_block        = element(var.private_subnet_cidrs, count.index)
-  availability_zone = element(var.azs, count.index)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+
   tags = {
-    Name = "eks-private-${count.index}"
+    Name = "${var.project}-private-${count.index}"
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
 
-# Public route table + association
 resource "aws_route_table" "public" {
-  vpc_id = data.aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
+
   tags = {
-    Name = "eks-public-rt"
+    Name = "${var.project}-public-rt"
   }
 }
 
-resource "aws_route" "default" {
+resource "aws_route" "internet_access" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.egw.id
 }
 
 resource "aws_route_table_association" "public" {
   count          = length(aws_subnet.public)
-  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
